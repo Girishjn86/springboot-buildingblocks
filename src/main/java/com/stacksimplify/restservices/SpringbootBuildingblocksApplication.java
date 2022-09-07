89cf2b59ab68001c5b8dfbf5bcfb4595bad7e028
girish <girishjn6@gmail.com>
2022-09-07 19:33:19 +0530

    Internationalization

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -1,7 +1,13 @@
 package com.stacksimplify.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.context.support.ResourceBundleMessageSource;
 //
 //
 //
@@ -10,6 +16,19 @@ public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localresolver = new AcceptHeaderLocaleResolver();
+		localresolver.setDefaultLocale(Locale.US);
+		return localresolver;
+		
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		return messageSource;
 	}
 
 }
--- src/main/java/com/stacksimplify/restservices/controller/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.stacksimplify.restservices.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stacksimplify.restservices.entities.UserDetails;
 
 @RestController
 public class HelloWorldController {
+	@Autowired
+	ResourceBundleMessageSource resourceBundleMessageSource;
 	@GetMapping("/helloworld")
 	public String helloWorld() {
 
@@ -14,8 +22,18 @@ public class HelloWorldController {
 		System.out.println("code of branch");
 		return "Hello World";
 	}
-	@GetMapping("helloworldbean")
+	@GetMapping("/helloworldbean")
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Girish", "Jarmali", "Chitradurga");
+	}
+	@GetMapping("/helloworld-int1")
+	public String getMessageInI18NFormat(@RequestHeader(name = "Accept-Language", required=false) String locale) {
+		return resourceBundleMessageSource.getMessage("lable.hello", null, new Locale(locale));
+		
+	}
+	@GetMapping("/helloworld-int2")
+	public String getMessageInI18NFormat2() {
+		return resourceBundleMessageSource.getMessage("lable.hello", null, LocaleContextHolder.getLocale());
+		
 	}
 }
